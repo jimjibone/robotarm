415568ec45a3bf4fd2923dec23b78d7942993e69
James Reuss <jamesreuss@gmail.com>
2013-02-25 15:15:20 +0000

    fix OKPCL for good times and workingness

--- object_detect/OKPCL.cpp
+++ object_detect/OKPCL.cpp
@@ -142,7 +142,7 @@ int main (int argc, char** argv)
 	// Create and setup the viewer
 	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
 	viewer->registerKeyboardCallback (keyboardEventOccurred, (void*)&viewer);
-	viewer->setBackgroundColor (0, 0, 0);
+	viewer->setBackgroundColor (255, 255, 255);
 	viewer->addPointCloud<pcl::PointXYZRGB> (cloud, "Kinect Cloud");
 	viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE,1, "Kinect Cloud");
 	viewer->addCoordinateSystem (1.0);
