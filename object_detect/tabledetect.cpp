45d4bbb83ecf2a02289d6dd46da43344925bd997
jimjibone <jamesreuss@gmail.com>
2013-03-13 00:49:54 +0000

    added required headers and build output after fail

--- object_detect/tabledetect.cpp
+++ object_detect/tabledetect.cpp
@@ -20,6 +20,13 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/console/parse.h>
 #include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/surface/convex_hull.h>
+#include <pcl/segmentation/extract_polygonal_prism_data.h>
+#include <pcl/segmentation/extract_clusters.h>
 
 #if defined(__APPLE__)
 #include <GLUT/glut.h>
