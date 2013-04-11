00e41cc85195e79a2119588897fdaaf148dabe07
jimjibone <jamesreuss@gmail.com>
2013-04-11 17:03:12 +0100

    attempt at getting pcl working again, failed

--- object_detect/basic.cpp
+++ object_detect/basic.cpp
@@ -16,6 +16,7 @@
 #include <ctime>
 #include <boost/thread/thread.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/shared_ptr.hpp>
 
 #include <pcl/common/common_headers.h>
 #include <pcl/io/pcd_io.h>
@@ -39,6 +40,10 @@
 
 
 
+using namespace std;
+using namespace pcl;
+
+
 
 /*---------------------------------------------*-
  * START THE ENGINES! IT'S BRIDGING TIME!
@@ -56,10 +61,18 @@ std::vector<uint16_t> mdepth(640*480);
 std::vector<uint8_t> mrgb(640*480*4);
 bool killKinect = false;
 // point clouds 'n' that.
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr table_plane_cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-//pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-//pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+PointCloud<PointXYZ>::Ptr cloud (new PointCloud<PointXYZ>);
+Eigen::VectorXf plane_coefficients (4);
+enum rotationType {
+	ABCD = 0,//normal
+	BACD,//ab
+	CBAD,//ac
+	DBCA,//ad
+	ACBD,//bc
+	ADCB,//bd
+	ABDC //cd
+};
+rotationType rotation = ABCD;
 
 /*---------------------------------------------*-
  * Table Detection Stuff
@@ -105,18 +118,19 @@ void drawCallback()
 	glBegin(GL_POINTS);
 	
 	for (unsigned int i = 0; i < cloud->size(); i++) {
-		glColor3ub(cloud->points[i].r, cloud->points[i].g, cloud->points[i].b);
+		//glColor3ub(cloud->points[i].r, cloud->points[i].g, cloud->points[i].b);
+		glColor3f(0.8, 0.8, 1.0);
 		glVertex3f(cloud->points[i].x, -cloud->points[i].y, -cloud->points[i].z);
 	}
 	
 	glEnd();
 	
 	if (done_ransac) {
-		/*glEnable(GL_BLEND);
+		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		
 		//show ransac indices
-		double boxSize = 300.0;
+		double boxSize = 600.0;
 		double bl_x = -boxSize, bl_y = -boxSize;
 		double tl_x = -boxSize, tl_y =  boxSize;
 		double tr_x =  boxSize, tr_y =  boxSize;
@@ -124,10 +138,61 @@ void drawCallback()
 		// ax + by + cz + d = 0
 		// ax + by + d = -cz
 		// -(ax + by + d)/c = z
-		double bl_z = -((coefficients->values[0]*bl_x) + (coefficients->values[1]*bl_y) + coefficients->values[3])/(coefficients->values[2]*1.0);
-		double tl_z = -((coefficients->values[0]*tl_x) + (coefficients->values[1]*tl_y) + coefficients->values[3])/(coefficients->values[2]*1.0);
-		double tr_z = -((coefficients->values[0]*tr_x) + (coefficients->values[1]*tr_y) + coefficients->values[3])/(coefficients->values[2]*1.0);
-		double br_z = -((coefficients->values[0]*br_x) + (coefficients->values[1]*br_y) + coefficients->values[3])/(coefficients->values[2]*1.0);
+		double bl_z = -((plane_coefficients[0]*bl_x) + (plane_coefficients[1]*bl_y) + plane_coefficients[3])/(plane_coefficients[2]*1.0);
+		double tl_z = -((plane_coefficients[0]*tl_x) + (plane_coefficients[1]*tl_y) + plane_coefficients[3])/(plane_coefficients[2]*1.0);
+		double tr_z = -((plane_coefficients[0]*tr_x) + (plane_coefficients[1]*tr_y) + plane_coefficients[3])/(plane_coefficients[2]*1.0);
+		double br_z = -((plane_coefficients[0]*br_x) + (plane_coefficients[1]*br_y) + plane_coefficients[3])/(plane_coefficients[2]*1.0);
+		
+		switch (rotation) {
+//				ABCD = 0,//normal
+//				BACD,//ab
+//				CBAD,//ac
+//				DBCA,//ad
+//				ACBD,//bc
+//				ADCB,//bd
+//				ABDC //cd
+			case BACD:
+				bl_z = -((plane_coefficients[1]*bl_x) + (plane_coefficients[0]*bl_y) + plane_coefficients[3])/(plane_coefficients[2]*1.0);
+				tl_z = -((plane_coefficients[1]*tl_x) + (plane_coefficients[0]*tl_y) + plane_coefficients[3])/(plane_coefficients[2]*1.0);
+				tr_z = -((plane_coefficients[1]*tr_x) + (plane_coefficients[0]*tr_y) + plane_coefficients[3])/(plane_coefficients[2]*1.0);
+				br_z = -((plane_coefficients[1]*br_x) + (plane_coefficients[0]*br_y) + plane_coefficients[3])/(plane_coefficients[2]*1.0);
+				break;
+			case CBAD:
+				bl_z = -((plane_coefficients[2]*bl_x) + (plane_coefficients[1]*bl_y) + plane_coefficients[0])/(plane_coefficients[2]*1.0);
+				tl_z = -((plane_coefficients[2]*tl_x) + (plane_coefficients[1]*tl_y) + plane_coefficients[0])/(plane_coefficients[2]*1.0);
+				tr_z = -((plane_coefficients[2]*tr_x) + (plane_coefficients[1]*tr_y) + plane_coefficients[0])/(plane_coefficients[2]*1.0);
+				br_z = -((plane_coefficients[2]*br_x) + (plane_coefficients[1]*br_y) + plane_coefficients[0])/(plane_coefficients[2]*1.0);
+				break;
+			case DBCA:
+				bl_z = -((plane_coefficients[3]*bl_x) + (plane_coefficients[1]*bl_y) + plane_coefficients[2])/(plane_coefficients[0]*1.0);
+				tl_z = -((plane_coefficients[3]*tl_x) + (plane_coefficients[1]*tl_y) + plane_coefficients[2])/(plane_coefficients[0]*1.0);
+				tr_z = -((plane_coefficients[3]*tr_x) + (plane_coefficients[1]*tr_y) + plane_coefficients[2])/(plane_coefficients[0]*1.0);
+				br_z = -((plane_coefficients[3]*br_x) + (plane_coefficients[1]*br_y) + plane_coefficients[2])/(plane_coefficients[0]*1.0);
+				break;
+			case ACBD:
+				bl_z = -((plane_coefficients[0]*bl_x) + (plane_coefficients[2]*bl_y) + plane_coefficients[1])/(plane_coefficients[3]*1.0);
+				tl_z = -((plane_coefficients[0]*tl_x) + (plane_coefficients[2]*tl_y) + plane_coefficients[1])/(plane_coefficients[3]*1.0);
+				tr_z = -((plane_coefficients[0]*tr_x) + (plane_coefficients[2]*tr_y) + plane_coefficients[1])/(plane_coefficients[3]*1.0);
+				br_z = -((plane_coefficients[0]*br_x) + (plane_coefficients[2]*br_y) + plane_coefficients[1])/(plane_coefficients[3]*1.0);
+				break;
+			case ADCB:
+				bl_z = -((plane_coefficients[0]*bl_x) + (plane_coefficients[3]*bl_y) + plane_coefficients[2])/(plane_coefficients[1]*1.0);
+				tl_z = -((plane_coefficients[0]*tl_x) + (plane_coefficients[3]*tl_y) + plane_coefficients[2])/(plane_coefficients[1]*1.0);
+				tr_z = -((plane_coefficients[0]*tr_x) + (plane_coefficients[3]*tr_y) + plane_coefficients[2])/(plane_coefficients[1]*1.0);
+				br_z = -((plane_coefficients[0]*br_x) + (plane_coefficients[3]*br_y) + plane_coefficients[2])/(plane_coefficients[1]*1.0);
+				break;
+			case ABDC:
+				bl_z = -((plane_coefficients[0]*bl_x) + (plane_coefficients[1]*bl_y) + plane_coefficients[3])/(plane_coefficients[2]*1.0);
+				tl_z = -((plane_coefficients[0]*tl_x) + (plane_coefficients[1]*tl_y) + plane_coefficients[3])/(plane_coefficients[2]*1.0);
+				tr_z = -((plane_coefficients[0]*tr_x) + (plane_coefficients[1]*tr_y) + plane_coefficients[3])/(plane_coefficients[2]*1.0);
+				br_z = -((plane_coefficients[0]*br_x) + (plane_coefficients[1]*br_y) + plane_coefficients[3])/(plane_coefficients[2]*1.0);
+				break;
+				
+			default:
+				//ABCD or Original
+				break;
+		}
+		
 		
 		glBegin(GL_QUADS);
 		glColor4f(.392156863, 1, .392156863, 0.3);
@@ -138,9 +203,9 @@ void drawCallback()
 		glVertex3d(br_x, -br_y, -br_z);
 		
 		
-		glEnd();*/
+		glEnd();
 		
-		glPointSize(1.0f);
+		/*glPointSize(1.0f);
 		glBegin(GL_POINTS);
 		
 		for (unsigned int i = 0; i < table_plane_cloud->size(); i++) {
@@ -149,7 +214,7 @@ void drawCallback()
 			glVertex3f(table_plane_cloud->points[i].x, -table_plane_cloud->points[i].y, -table_plane_cloud->points[i].z);
 		}
 		
-		glEnd();
+		glEnd();*/
 	}
 	
 	
@@ -197,9 +262,9 @@ void glutIdleCallback()
 				cloud->points[i].x = x;
 				cloud->points[i].y = y;
 				cloud->points[i].z = iRealDepth;
-				cloud->points[i].r = mrgb[i*3];
-				cloud->points[i].g = mrgb[(i*3)+1];
-				cloud->points[i].b = mrgb[(i*3)+2];
+				//cloud->points[i].r = mrgb[i*3];
+				//cloud->points[i].g = mrgb[(i*3)+1];
+				//cloud->points[i].b = mrgb[(i*3)+2];
 			}
 		}
 		
@@ -233,54 +298,47 @@ void glutIdleCallback()
 		done_ransac = false;
 		done_detection = false;
 		
-		// Create the segmentation object and do RANSAC
-		/*pcl::SACSegmentation<pcl::PointXYZRGB> seg;
-		 seg.setModelType(pcl::SACMODEL_PLANE);
-		 seg.setMethodType(pcl::SAC_RANSAC);
-		 seg.setDistanceThreshold(0.01);
-		 seg.setInputCloud(cloud);
-		 seg.segment(*inliers, *coefficients);
-		 std::cerr << "PointCloud after segmentation has: " << inliers->indices.size () << " inliers." << std::endl;
-		 std::cerr << "Plane coefficients:  a:" << coefficients->values[0] <<
-		 "  b:" << coefficients->values[1] <<
-		 "  c:" << coefficients->values[2] <<
-		 "  d:" << coefficients->values[3] << std::endl;
-		 
-		 pcl::sample
-		 
-		 done_ransac = true;
-		 
-		 double boxSize = 300.0;
-		 double bl_x = -boxSize, bl_y = -boxSize;
-		 double tl_x = -boxSize, tl_y =  boxSize;
-		 double tr_x =  boxSize, tr_y =  boxSize;
-		 double br_x =  boxSize, br_y = -boxSize;
-		 // ax + by + cz + d = 0
-		 // ax + by + d = -cz
-		 // -(ax + by + d)/c = z
-		 double bl_z = -((coefficients->values[0]*bl_x) + (coefficients->values[1]*bl_y) + coefficients->values[3])/(coefficients->values[2]*1.0);
-		 double tl_z = -((coefficients->values[0]*tl_x) + (coefficients->values[1]*tl_y) + coefficients->values[3])/(coefficients->values[2]*1.0);
-		 double tr_z = -((coefficients->values[0]*tr_x) + (coefficients->values[1]*tr_y) + coefficients->values[3])/(coefficients->values[2]*1.0);
-		 double br_z = -((coefficients->values[0]*br_x) + (coefficients->values[1]*br_y) + coefficients->values[3])/(coefficients->values[2]*1.0);
-		 
-		 std::cerr << "Plane in 3D is  BL: x:" << bl_x << "  y:" << bl_y << "  z:" << bl_z << std::endl;
-		 std::cerr << "                TL: x:" << tl_x << "  y:" << tl_y << "  z:" << tl_z << std::endl;
-		 std::cerr << "                TR: x:" << tr_x << "  y:" << tr_y << "  z:" << tr_z << std::endl;
-		 std::cerr << "                BR: x:" << br_x << "  y:" << br_y << "  z:" << br_z << std::endl;
-		 std::cerr << "Off-Mid Point (" << (size_t)(cloud->size()/2+(640*100+46)) <<
-		 ") is  x:" << cloud->points[(size_t)(cloud->size()/2+(640*100+46))].x <<
-		 "  y:" << cloud->points[(size_t)(cloud->size()/2+(640*100+46))].y <<
-		 "  z:" << cloud->points[(size_t)(cloud->size()/2+(640*100+46))].z << std::endl;*/
-		
+		// Perform the RANSAC algorithm!
 		// http://pointclouds.org/documentation/tutorials/random_sample_consensus.php
-		std::vector<int> inliers;
-		pcl::SampleConsensusModelPlane<pcl::PointXYZRGB>::Ptr planeModel (new pcl::SampleConsensusModelPlane<pcl::PointXYZRGB> (cloud));
-		pcl::RandomSampleConsensus<pcl::PointXYZRGB> ransac (planeModel);
-		ransac.setDistanceThreshold(0.01);
-		ransac.computeModel();
-		ransac.getInliers(inliers);
-		// copy all the inliers of the model to another point cloud.
-		pcl::copyPointCloud<pcl::PointXYZRGB>(*cloud, inliers, *table_plane_cloud);
+		// PCL-segmentation.pdf
+		// Create a shared plane model pointer directly
+		SampleConsensusModelPlane<PointXYZ>::Ptr model (new SampleConsensusModelPlane<PointXYZ>(cloud));
+		// Create the RANSAC object
+		RandomSampleConsensus<PointXYZ> sac(model, 0.03); // 3cm
+		// Perform the segmentation step
+		bool result = sac.computeModel();
+		if (result) {
+			cout << "RANSAC computeModel() was successful" << endl;
+		} else {
+			cout << "RANSAC computeModel() was UNsuccessful" << endl;
+		}
+		// Get inlier indices
+		boost::shared_ptr<vector<int> > inliers (new vector<int>);
+		sac.getInliers(*inliers);	// retrieve the best set of inliers
+		cout << "Found model with " << inliers->size() << " inliers";
+		// Get model coefficients
+		Eigen::VectorXf coeff;
+		sac.getModelCoefficients(coeff);	// ..and the corresponding plane model coefficients
+		cout << ". plane normal is: " << coeff[0] << ", " << coeff[1] << ", " << coeff[2] << ", " << coeff[3] << endl;
+		// Then some optional stuff
+		// Perform a refitting step
+		Eigen::VectorXf coeff_refined;
+		model->optimizeModelCoefficients(*inliers, coeff, coeff_refined);
+		model->selectWithinDistance(coeff_refined, 0.03, *inliers);
+		cout << "After refitting, model contains " << inliers->size() << " inliers";
+		cout << ", plane normal is: " << coeff_refined[0] << ", "
+									  << coeff_refined[1] << ", "
+									  << coeff_refined[2] << ", "
+									  << coeff_refined[3] << "." << endl;
+		// Projection
+		PointCloud<PointXYZ> proj_points;
+		model->projectPoints(*inliers, coeff_refined, proj_points);
+		// Set the global coefficients
+		plane_coefficients[0] = coeff_refined[0];
+		plane_coefficients[1] = coeff_refined[1];
+		plane_coefficients[2] = coeff_refined[2];
+		plane_coefficients[3] = coeff_refined[3];
+		
 		done_ransac = true;
 		
 		if (show_table_cloud) {
@@ -337,6 +395,21 @@ void keyboardCallback(unsigned char key, int x, int y)
 		print_midpoint = true;
 	if (key == 'd')
 		do_detection = true;
+	if (key == 'c') {
+//				ABCD = 0,//normal
+//				BACD,//ab
+//				CBAD,//ac
+//				DBCA,//ad
+//				ACBD,//bc
+//				ADCB,//bd
+//				ABDC //cd
+		if (rotation == ABDC) {
+			rotation = ABCD;
+		} else {
+			rotation++;
+		}
+		cout << "rotating the plane coefficients to type " << rotation << endl;
+	}
 }
 void movementCallback(int x, int y)
 {
