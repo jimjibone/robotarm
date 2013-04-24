207853e545dd8993183d73e28ac81763cc7641ac
Ian Chapple <i.chapple@live.co.uk>
2013-04-24 11:10:23 +0100

    Opps...

--- EyeTracker/Calibration/Quadratic.cpp
+++ EyeTracker/Calibration/Quadratic.cpp
@@ -38,39 +38,7 @@ void Quadratic::FindLine(int Xi[], int Yi[], int n)
               Db2 = TX3 * (TX2Y * n - TY * TX2),
                     Db3 = TX2 * (TX2Y * TX - TXY * TX2);
     int Db = Db1 - Db2 + Db3;
-    int Dc1 = TX4 * (TX2 * TY - TX * TXY)int TX4 = 0, TX3 = 0, TX2 = 0, TX = 0, TX2Y = 0, TXY = 0, TY = 0;
-    for (int cnt = 0; cnt < n; cnt++)
-    {
-        TX4 += Pow(Xi[cnt], 4);
-        TX3 += Pow(Xi[cnt], 3);
-        TX2 += Pow(Xi[cnt], 2);
-        TX += Xi[cnt];
-        TX2Y += Pow(Xi[cnt], 2) * Yi[cnt];
-        TXY += Xi[cnt] * Yi[cnt];
-        TY += Yi[cnt];
-    }
-
-    int D1 = TX4 * (TX2 * n - Pow(TX, 2)),
-             D2 = TX3 * (TX3 * n - TX * TX2),
-                  D3 = TX2 * (TX3 * TX - Pow(TX2, 2));
-    int D = D1 - D2 + D3;
-    int Da1 = TX2Y * (TX2 * n - Pow(TX, 2)),
-              Da2 = TXY * (TX3 * n - TX * TX2),
-                    Da3 = TY * (TX3 * TX - Pow(TX2, 2));
-    int Da = Da1 - Da2 + Da3;
-    int Db1 = TX4 * (TXY * n - TY * TX),
-              Db2 = TX3 * (TX2Y * n - TY * TX2),
-                    Db3 = TX2 * (TX2Y * TX - TXY * TX2);
-    int Db = Db1 - Db2 + Db3;
     int Dc1 = TX4 * (TX2 * TY - TX * TXY),
-              Dc2 = TX3 * (TX3 * TY - TX * TX2Y),
-                    Dc3 = TX2 * (TX3 * TXY - TX2 * TX2Y);
-    int Dc = Dc1 - Dc2 + Dc3;
-
-
-    double a = (double)Da / (double)D;
-    double b = (double)Db / (double)D;
-    double c = (double)Dc / (double)D;,
               Dc2 = TX3 * (TX3 * TY - TX * TX2Y),
                     Dc3 = TX2 * (TX3 * TXY - TX2 * TX2Y);
     int Dc = Dc1 - Dc2 + Dc3;
@@ -106,20 +74,20 @@ void Quadratic::FindLine(double[] Xi,double[] Yi, int n)
     }
 
     double D1 = TX4 * (TX2 * n - Pow(TX, 2)),
-             D2 = TX3 * (TX3 * n - TX * TX2),
-                  D3 = TX2 * (TX3 * TX - Pow(TX2, 2));
+                D2 = TX3 * (TX3 * n - TX * TX2),
+                     D3 = TX2 * (TX3 * TX - Pow(TX2, 2));
     double D = D1 - D2 + D3;
     double Da1 = TX2Y * (TX2 * n - Pow(TX, 2)),
-              Da2 = TXY * (TX3 * n - TX * TX2),
-                    Da3 = TY * (TX3 * TX - Pow(TX2, 2));
+                 Da2 = TXY * (TX3 * n - TX * TX2),
+                       Da3 = TY * (TX3 * TX - Pow(TX2, 2));
     double Da = Da1 - Da2 + Da3;
     double Db1 = TX4 * (TXY * n - TY * TX),
-              Db2 = TX3 * (TX2Y * n - TY * TX2),
-                    Db3 = TX2 * (TX2Y * TX - TXY * TX2);
+                 Db2 = TX3 * (TX2Y * n - TY * TX2),
+                       Db3 = TX2 * (TX2Y * TX - TXY * TX2);
     double Db = Db1 - Db2 + Db3;
     double Dc1 = TX4 * (TX2 * TY - TX * TXY),
-              Dc2 = TX3 * (TX3 * TY - TX * TX2Y),
-                    Dc3 = TX2 * (TX3 * TXY - TX2 * TX2Y);
+                 Dc2 = TX3 * (TX3 * TY - TX * TX2Y),
+                       Dc3 = TX2 * (TX3 * TXY - TX2 * TX2Y);
     double Dc = Dc1 - Dc2 + Dc3;
 
 
